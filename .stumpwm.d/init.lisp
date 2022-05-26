8da6a10523ff017f8fb20650c1e0079d5781b5ec
Henrik Hörmann <leinfink@mailbox.org>
2022-05-26 20:21:37 +0200

    Add StumpWM config

---
+++ .stumpwm.d/init.lisp
@@ -0,0 +1,29 @@
+(in-package :stumpwm)
+
+(require :swank)
+(swank-loader:init)
+(swank:create-server :port 4004
+                     :style swank:*communication-style*
+                     :dont-close t)
+
+(setf *mouse-focus-policy* :click)
+
+(define-remapped-keys
+    '(("(firefoxdeveloperedition|Chrome)" 
+       ("C-n"   . "Down")
+       ("C-p"   . "Up")
+       ("C-f"   . "Right")
+       ("C-b"   . "Left")
+       ("C-v"   . "Next")
+       ("M-v"   . "Prior")
+       ("M-w"   . "C-c")
+       ("C-w"   . "C-x")
+       ("C-y"   . "C-v")
+       ("M-<"   . "Home")
+       ("M->"   . "End")
+       ("C-M-b" . "M-Left")
+       ("C-M-f" . "M-Right")
+       ("C-k"   . ("C-S-End" "C-x")))))
+
+(define-key *root-map* (kbd "C-q") "send-raw-key")
+
